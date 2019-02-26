e0d723767987a9462bdd5a22dedee9eb6ef85f27
neomiost <neomiost@gmail.com>
2019-02-26 09:11:11 +0200

    starting!!!

---
+++ Emergency/Emergency/ViewModels/MainVM.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergency.ViewModels
+{
+   
+    public class MainVM:BaseVM
+    {
+        private AnalysisVM analysisVM;
+        public AnalysisVM AnalysisVM
+        {
+            get
+            {
+                return analysisVM;
+            }
+            set
+            {
+                analysisVM = value;
+                OnPropertyRaised("AnalysisVM");
+            }
+        }
+        private CollectDetailsVM collectDetailsVM;
+        public CollectDetailsVM CollectDetailsVM
+        {
+            get
+            {
+                return collectDetailsVM;
+            }
+            set
+            {
+                collectDetailsVM = value;
+                OnPropertyRaised("CollectDetailsVM");
+            }
+        }
+        private IdentificationVM identificationVM;
+        public IdentificationVM IdentificationVM
+        {
+            get
+            {
+                return identificationVM;
+            }
+            set
+            {
+                identificationVM = value;
+                OnPropertyRaised("IdentificationVM");
+            }
+        }
+        private UpdateResultsVM udateResultsVM;
+        public UpdateResultsVM UpdateResultsVM
+        {
+            get
+            {
+                return udateResultsVM;
+            }
+            set
+            {
+                udateResultsVM = value;
+                OnPropertyRaised("UpdateResultsVM");
+            }
+        }
+        private ViewDetailsVM viewDetailsVM;
+        public ViewDetailsVM ViewDetailsVM
+        {
+            get
+            {
+                return viewDetailsVM;
+            }
+            set
+            {
+                viewDetailsVM = value;
+                OnPropertyRaised("ViewDetailsVM");
+            }
+        }
+        public MainVM()
+        {
+            AnalysisVM = new ViewModels.AnalysisVM();
+            CollectDetailsVM = new ViewModels.CollectDetailsVM();
+            IdentificationVM = new IdentificationVM();
+            UpdateResultsVM = new UpdateResultsVM();
+            ViewDetailsVM = new ViewDetailsVM();
+
+        }
+    }
+}
---
+++ Emergency/Emergency/Views/CollectDetailsUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emergency.Views
+{
+    /// <summary>
+    /// Interaction logic for InitDetailsUC.xaml
+    /// </summary>
+    public partial class CollectDetailsUC : UserControl
+    {
+        public CollectDetailsUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
